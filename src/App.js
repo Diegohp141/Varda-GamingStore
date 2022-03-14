4921abc4e7220af52b79fad9ae59e2cff86695db
Diego_Piola <diegohernanpiola@gmail.com>
2022-03-14 19:09:13 -0300

    NavBar continuacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
     </div>
   );
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,29 +1,47 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+import './NavBar.css';
 
 function NavBar() {
-  return (
-    <div>
-      <Navbar bg="dark" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-      </Navbar>
-    </div>
+  return (    
+    <Navbar className="container-fluid" bg="black" expand="lg" variant="dark">        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="PlayStation" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Consolas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Videojuegos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Accesorios</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Xbox" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Consolas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Videojuegos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Accesorios</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Nintendo" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Consolas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Videojuegos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Accesorios</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Brand className="textNav" href="#home">Varda´s Gaming Store</Navbar.Brand>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+    </Navbar>
   )
 }
 
